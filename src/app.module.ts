ab9d8ae23ef9df141fa84f61a4c963b2794470d7
Daliane <daliane.rabelo01@gmail.com>
2025-08-08 11:10:16 -0300

    Atualiza BlogPessoal e configura o ambiente de testes

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { Tema } from './tema/entities/tema.entity';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { PostagemModule } from './postagem/postagem.module';
+import { TemaModule } from './tema/tema.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '3112',
+      database: 'db_blogpessoal',
+      entities: [Postagem, Tema, Usuario],
+      synchronize: true,
+      logging: true,
+    }),
+    PostagemModule,
+    TemaModule,
+    AuthModule,
+    UsuarioModule,
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '+TLLdpUjcGdtBv/pHo1+jwVWdrcjdsn5XTZcEYidEF6r5vi020j66c3msF7IDj5l',
+};
